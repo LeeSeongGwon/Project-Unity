d1502b1671cba270584a6a0b68ab7f5e9968842b
SeongGwon <141493545+LeeSeongGwon@users.noreply.github.com>
2023-09-07 17:01:47 +0900

    Add files via upload

---
+++ TopDownCharacterController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
+    {
+        OnMoveEvent?.Invoke(direction);
+    }
+
+    public void CallLookEvent(Vector2 direction)
+    {
+        OnLookEvent?.Invoke(direction);
+    }
+
+
+
+}
+    //[SerializeField] private float speed = 5f;
+    // Start is called before the first frame update
+   // void Start()
+    
+        
+    
+
+    // Update is called once per frame
+    //void Update()
+    
+        //Drag , Ctrl + K C 주석 K U 풀기
+        //float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+
+        //transform.position += new Vector3(x, y) * Time.deltaTime;
+    
+
